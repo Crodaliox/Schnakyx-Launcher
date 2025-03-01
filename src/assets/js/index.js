1bd6c03120409a1c7307fd432b7dc51f809011af
Crodaliox <etienne.etiennejulien@gmail.com>
2025-03-01 09:41:05 +0100

    V0.0.2

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,8 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Bonne chance pour votre mission ! ", "author": "EOF" },
+            { "message": "Pourquoi il y a t il tant de monstre ?", "author": "EOF" },
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
