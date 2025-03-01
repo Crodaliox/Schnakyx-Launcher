01bad2ba2c680eb86e1e842e771732ab605da0f5
Crodaliox <etienne.etiennejulien@gmail.com>
2025-03-01 01:07:53 +0100

    V 0.0.1 Schnakyx Launcher

---
+++ Selvania-Launcher-WEB-Folder/Selvania-Launcher-WEB-Folder/files/php/instances.php
@@ -0,0 +1,28 @@
+<?php
+
+
+$instance['Schnakyx'] = array_merge($instance['Schnakyx'], array(
+    "loadder" => array(
+        "minecraft_version" => "1.20.1",
+        "loadder_type" => "forge",
+        "loadder_version" => "latest"
+    ),
+    "verify" => false,
+    "ignored" => array(
+        'config',
+        'essential',
+        'logs',
+        'resourcepacks',
+        'saves',
+        'screenshots',
+        'shaderpacks',
+        'W-OVERFLOW',
+        'options.txt',
+        'optionsof.txt'
+    ),
+    "whitelist" => array(),
+    "whitelistActive" => false,
+    "status" => array(
+    )
+));
+?>
